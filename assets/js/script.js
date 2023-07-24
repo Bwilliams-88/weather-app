c821a6c7d4a5a1bf5c238d8047c020527382cfdc
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-07-24 17:56:12 -0500

    got my 'starter' code added, still need to work on the weather icons and layout of the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const apiKey = "YOUR_OPENWEATHERMAP_API_KEY";
-    const fetchButton = document.getElementById("fetchButton");
-    const locationInput = document.getElementById("locationInput");
-    const weatherInfo = document.getElementById("weatherInfo");
+    var apiKey = "a39d12d193c78d9930f104a6c6fb6d98";
+    var weatherInfo = document.getElementById('weatherInfo');
+    var searchHistory = document.getElementById('searchHistory');
   
-    fetchButton.addEventListener("click", () => {
-      const location = locationInput.value.trim();
-      if (location === "") {
-        alert("Please enter a location.");
-        return;
-      }
-  
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
-        location
-      )}&appid=${apiKey}&units=metric`;
-  
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          const { name, main, weather } = data;
-          const temperature = main.temp;
-          const description = weather[0].description;
-          weatherInfo.innerHTML = `<p>Location: ${name}</p>
-                                   <p>Temperature: ${temperature}°C</p>
-                                   <p>Weather: ${description}</p>`;
+    function getWeather(city) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+          const cityName = data.name;
+          const temperature = data.main.temp;
+          const description = data.weather[0].description;
+          const weatherData = `${cityName}: ${temperature}°C, ${description}`;
+          weatherInfo.innerText = weatherData;
+    
+          // Add city to the search history
+          const historyItem = document.createElement('p');
+          historyItem.innerText = cityName;
+          searchHistory.appendChild(historyItem);
         })
-        .catch((error) => {
-          console.error("Error fetching weather data:", error);
-          weatherInfo.innerHTML = "<p>Failed to fetch weather data.</p>";
-        });
-    });
-  });
+        
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+        weatherInfo.innerText = 'City not found or error fetching data';
+      });
+  }
   
+  function searchWeather() {
+    const cityInput = document.getElementById('cityInput');
+    const city = cityInput.value;
+    getWeather(city);
+    cityInput.value = '';
+  }
+  
+  document.getElementById('cityInput').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      searchWeather();
+    }
+  });
+
+   
+
+
+  //  var apiKey = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a39d12d193c78d9930f104a6c6fb6d98}";
