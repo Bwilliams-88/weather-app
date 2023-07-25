f5b0279571be23cd21219a7fda92382c7109b4d7
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-07-25 17:49:22 -0500

    still working on future forecast and weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@
     var searchHistory = document.getElementById('searchHistory');
   
     function getWeather(city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
         .then(response => response.json())
         .then(data => {
           const cityName = data.name;
           const temperature = data.main.temp;
           const description = data.weather[0].description;
-          const weatherData = `${cityName}: ${temperature}°C, ${description}`;
+          const weatherData = `${cityName}: ${temperature}°F, ${description}`;
           weatherInfo.innerText = weatherData;
     
           // Add city to the search history
@@ -30,14 +30,48 @@
     getWeather(city);
     cityInput.value = '';
   }
+
+  function futureWeather(apiUrl) {
+    // extracts future forecast data
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={cityname},{statecode},{countrycode}&limit={limit}&appid={APIkey}`)
+    .then(response => response.json())
+    .then(data => {
+      const futureForecast = data.list.filter(item => {
+        const forecastDate = new Date(item.dt * 1000);
+        return forecastDate > new Date();
+      });
+      
+    });
+
+    // displays future forecast info
+    const forecastContainer = document.getElementById('forecast-container');
+    futureForecast.forEach(item => {
+      const forecastDate = new Date(item => {
+        const forecastDate = new Date(item.dt * 1000);
+        const temperature = item.main.temp;
+        const description = item.weather[0].description;
+
+        const forecastCard = document.createElement('div');
+        forecastCard.innerHTML = `
+        <h3>${forecastDate.toLocaleString()}</h3>
+        <p>Temperature: ${temperature} &#176;F</p>
+        <p>Description: ${description}</p>
+        `;
+
+        forecastContainer.appendChild(forecastCard);
+      })
+    })
+    .catch(error => console.log('Error fetching forecast:', error));
+  }
   
   document.getElementById('cityInput').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
       searchWeather();
     }
   });
+  
 
    
 
 
-  //  var apiKey = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a39d12d193c78d9930f104a6c6fb6d98}";
+  //  var apiKey = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}&units=imperial";
