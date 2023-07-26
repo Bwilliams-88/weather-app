669cc01ca9946622f1a6254b3e4d77a5334d1a09
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-07-25 21:00:09 -0500

    redid code in html and js files to be more understandable for myself

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,86 @@
     var apiKey = "a39d12d193c78d9930f104a6c6fb6d98";
-    var weatherInfo = document.getElementById('weatherInfo');
-    var searchHistory = document.getElementById('searchHistory');
-  
-    function getWeather(city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
-        .then(response => response.json())
-        .then(data => {
-          const cityName = data.name;
-          const temperature = data.main.temp;
-          const description = data.weather[0].description;
-          const weatherData = `${cityName}: ${temperature}°F, ${description}`;
-          weatherInfo.innerText = weatherData;
+    const searchHistory = [];
+
+    function searchWeather() {
+      const cityInput = document.getElementById('cityInput').value;
+      fetchWeatherData(cityInput)
+        .then((data) => {
+          // Display current weather data
+          displayCurrentWeather(data);
     
-          // Add city to the search history
-          const historyItem = document.createElement('p');
-          historyItem.innerText = cityName;
-          searchHistory.appendChild(historyItem);
+          // Display 5-day forecast
+          displayForecast(data);
+    
+          // Add the city to search history
+          addToSearchHistory(cityInput);
         })
-        
-      .catch(error => {
-        console.error('Error fetching weather data:', error);
-        weatherInfo.innerText = 'City not found or error fetching data';
-      });
-  }
-  
-  function searchWeather() {
-    const cityInput = document.getElementById('cityInput');
-    const city = cityInput.value;
-    getWeather(city);
-    cityInput.value = '';
-  }
-
-  function futureWeather(apiUrl) {
-    // extracts future forecast data
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={cityname},{statecode},{countrycode}&limit={limit}&appid={APIkey}`)
-    .then(response => response.json())
-    .then(data => {
-      const futureForecast = data.list.filter(item => {
-        const forecastDate = new Date(item.dt * 1000);
-        return forecastDate > new Date();
-      });
-      
-    });
-
-    // displays future forecast info
-    const forecastContainer = document.getElementById('forecast-container');
-    futureForecast.forEach(item => {
-      const forecastDate = new Date(item => {
-        const forecastDate = new Date(item.dt * 1000);
-        const temperature = item.main.temp;
-        const description = item.weather[0].description;
-
-        const forecastCard = document.createElement('div');
-        forecastCard.innerHTML = `
-        <h3>${forecastDate.toLocaleString()}</h3>
-        <p>Temperature: ${temperature} &#176;F</p>
-        <p>Description: ${description}</p>
-        `;
-
-        forecastContainer.appendChild(forecastCard);
-      })
-    })
-    .catch(error => console.log('Error fetching forecast:', error));
-  }
-  
-  document.getElementById('cityInput').addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
+        .catch((error) => {
+          console.error('Error fetching weather data:', error);
+        });
+    }
+    
+    async function fetchWeatherData(city) {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        throw new Error('Failed to fetch weather data');
+      }
+    }
+    
+    function displayCurrentWeather(data) {
+      // Extract necessary data from the API response
+      const city = data.name;
+      const date = new Date(data.dt * 1000).toLocaleDateString();
+      const icon = data.weather[0].icon;
+      const temperature = data.main.temp;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+    
+      // Display current weather data in the HTML element
+      const currentWeatherDiv = document.getElementById('currentWeather');
+      currentWeatherDiv.innerHTML = `
+        <h2>${city} - ${date}</h2>
+        <img src="https://openweathermap.org/img/w/${icon}.png" alt="Weather Icon">
+        <p>Temperature: ${temperature} °C</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
+      `;
+    }
+    
+    function displayForecast(data) {
+      // Fetch 5-day forecast data using another API endpoint (not shown here)
+      // You can use the OpenWeatherMap API again to fetch the forecast data
+    
+      // Once you have the forecast data, loop through it and display each day's forecast
+      // in the "forecast" div using a similar approach as displayCurrentWeather()
+    }
+    
+    function addToSearchHistory(city) {
+      if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+    
+        // Update the search history display
+        const searchHistoryDiv = document.getElementById('searchHistory');
+        searchHistoryDiv.innerHTML = searchHistory.map(city => `<p onclick="searchPreviousCity('${city}')">${city}</p>`).join('');
+      }
+    }
+    
+    function searchPreviousCity(city) {
+      document.getElementById('cityInput').value = city;
       searchWeather();
     }
-  });
+
   
+    document.getElementById('cityInput').addEventListener('keydown', function (event) {
+      if (event.key === 'Enter') {
+        searchWeather();
+      }
+    });
 
    
 
 
   //  var apiKey = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}&units=imperial";
+  //  var latLonApit = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
